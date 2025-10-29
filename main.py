13ceb76cd599a08bf50f166aa557b8a34745693f
adi <adi.takhar@gmail.com>
2025-10-28 21:36:29 -0400

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -9,8 +9,30 @@ screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Echo Maze (Player Pulse)")
 
 # Player setup
-player = pygame.Rect(300, 300, 50, 50)
+player = pygame.Rect(20, 20, 30, 30)
 echoes = []
+
+# Maze Genreation
+def genMaze(width, height):
+    # Grid of walls
+    maze = [[1 for _  in range(width)] for _ in range(height)]
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)] # up, right, down, left
+
+    def carve(x, y):
+        maze[x][y] = 0
+        random.shuffle(directions)
+        for dx, dy in directions:
+            nx, ny = (x + dx * 2), (y + dy * 2)
+
+            if 0 <= ny < height and 0 <= nx < width and maze[ny][nx] == 1:
+                maze[y + dy][x + dx] = 0
+                carve(nx,ny)
+    
+
+    carve(0, 0)
+    maze[0][0] = 0
+    maze[height - 1][width - 1] = 0
+    return maze
 
 run = True
 while run:
@@ -23,7 +45,7 @@ while run:
             echoes.append([player.centerx, player.centery, 0, 255])
 
     # Background color
-    screen.fill((0, 20, 120))
+    screen.fill((0, 0, 0))
 
     # Movement speed
     key = pygame.key.get_pressed()
@@ -34,7 +56,7 @@ while run:
         x = 2
         y = -2
 
-    # Diagonal movement (your method)
+    # Diagonal movement
     if key[pygame.K_w] and key[pygame.K_d]:
         player.move_ip(x * 0.7071, y * 0.7071)
     elif key[pygame.K_d] and key[pygame.K_s]:
